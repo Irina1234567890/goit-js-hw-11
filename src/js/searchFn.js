cc6c40ceb28a31e6d07d6ff2e77c07831254e31b
Irina1234567890 <irina3213@gmail.com>
2022-07-25 11:15:15 +0300

    change

---
+++ src/js/searchFn.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+
+export default function searchRes(inputTxt, page) {
+  const keyAPI = '28108593-121c85f8532d16352eac042b7';
+
+  return axios
+    .get(
+      `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${inputTxt}&page=${page}&per_page=40&key=${keyAPI}`
+    )
+    .then(res => res.data);
+}
